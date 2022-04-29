6f4ee5488b7ab3d6d2ac01fe8be1e79a52805b6d
jeffpontocom <jeffpontocom@gmail.com>
2022-04-29 16:42:24 -0300

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,21 +30,17 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "613e4cc1af0eb5148b8ce409ad35446d",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "dd3c4233029270506ecc994d67785a37",
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
-"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "favicon.png": "d9f0bfa8d2610aa0c02ab61c04f57df6",
 "firebase-messaging-sw.js": "a442c72dd7910b5de634ba1ce79a6fa2",
 "icons/Icon-192.png": "701e43ae3eb8ad390c9b65537bc8a169",
 "icons/Icon-512.png": "51cbe3b1cb0ac7ddd62f3833cba6f39a",
 "icons/Icon-maskable-192.png": "701e43ae3eb8ad390c9b65537bc8a169",
 "icons/Icon-maskable-512.png": "51cbe3b1cb0ac7ddd62f3833cba6f39a",
-"index.html": "17b8baa0dc9287a67766a6b2459359cc",
-"/": "17b8baa0dc9287a67766a6b2459359cc",
-"main.dart.js": "4e932ef6d98ba2a88eab0cab36940714",
+"index.html": "d5f015989c640c967db5a6b2d5df5ecb",
+"/": "d5f015989c640c967db5a6b2d5df5ecb",
+"main.dart.js": "4ff63e6905a6e1f0bb910834cca9ce04",
 "manifest.json": "7051455cda8feb95c100f8203fb1431e",
-"version.json": "bcb9de25784e2e658f5762f43c56e97a"
+"version.json": "804c96ce727d4897d38abc937c51f07d"
 };
 
 // The application shell files that are downloaded before a service worker can
